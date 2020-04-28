9d6fc7829f0dcd69ddba7b3dc529af4f0cdf6d20
JulianLozanoG <jalozanog93@hotmail.com>
2020-04-28 10:49:48 -0500

    Basic versionaning

---
+++ src/main/java/com/jlozano/restwebservicemicro/versioning/PersonVersioningController.java
@@ -0,0 +1,19 @@
+package com.jlozano.restwebservicemicro.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonVersioningController {
+	
+	@GetMapping("v1/persons")
+	public PersonV1 personV1() {
+		return new PersonV1("David Lozano");
+	}
+	
+	@GetMapping("v2/persons")
+	public PersonV2 personV2() {
+		return new PersonV2(new Name("David","Lozano"));
+	}
+	
+}
