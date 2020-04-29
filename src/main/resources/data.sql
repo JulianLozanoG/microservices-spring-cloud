3630383edf8a9db9c3ce03105a2201808c726d00
JulianLozanoG <jalozanog93@hotmail.com>
2020-04-29 10:14:09 -0500

    Using of oneToMany and ManyToOne to build the relations between tables in JPA

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-insert into user values (1001, sysdate(), 'AB');
-insert into user values (1002, sysdate(), 'Julian');
-insert into user values (1003, sysdate(), 'Angelica');
+insert into user values (10001, sysdate(), 'AB');
+insert into user values (10002, sysdate(), 'Julian');
+insert into user values (10003, sysdate(), 'Angelica');
+insert into post values (11001,'My first POST', 10001);
+insert into post values (11002,'My second POST', 10001);
